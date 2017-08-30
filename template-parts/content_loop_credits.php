11fdfc2007155d8bca172328e546416462c93389
salwa <salwaoudad@gmail.com>
2017-08-30 15:07:18 +0200

    add icons -> Credits //

--- template-parts/content_loop_credits.php
+++ template-parts/content_loop_credits.php
@@ -3,14 +3,16 @@
 foreach ($posts as $post) {
     setup_postdata( $post );
     $tags = wp_get_post_tags($post->ID);
+    $icons = get_post_meta($post->ID, 'icon', true);
     ?>
         <article class="col-md-6 col-sm-6 credits">
             <div class="">
-                <i class="fa fa-desktop" aria-hidden="true"></i>
-            </div>
+
+                <?php echo '<i class="fa '.$icons.'" aria-hidden="true"></i></div>'
+                ?>
 
             <div>
-                <h3><?php the_title(); ?></h3>
+                <h3><?php the_title();?></h3>
                 <?php the_content(); ?>
             </div>
         </article>
